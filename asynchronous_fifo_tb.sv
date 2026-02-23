f5700d4e8c9f05af3c7a94a7613871b2725d2808
Vidhula Pallavor <78312866+Vidhula-P@users.noreply.github.com>
2026-02-23 15:24:21 -0500

    correct min depth

--- asynchronous_fifo_tb.sv
+++ asynchronous_fifo_tb.sv
@@ -1,17 +1,21 @@
-// minimum depth ≈ 2 x clock ratio
-// Hence FIFO DEPTH > 4 
+// burst length = 16 words
+// write frequency = 10 ns
+// read  frequency = 20 ns
+// Hence, minimum FIFO depth = 16 - (16 x 10)/20 = 8
 
 module asynchronous_fifo_tb;
   
   parameter DEPTH = 8;
   parameter WIDTH = 8;
+  parameter BURST_LENGTH = 10;
   
   logic wr_clk, rd_clk, reset, write_en, read_en;
-  logic  [WIDTH-1:0] write_data; 
+  logic [WIDTH-1:0] write_data; 
   logic [WIDTH-1:0] read_data;
   logic full, empty;
   
   int i; // loop variable
+  int burst_counter;
   
   asynchronous_fifo #(DEPTH, WIDTH) f1 (wr_clk, rd_clk, reset, 
                            write_en, write_data,
@@ -28,9 +32,6 @@ module asynchronous_fifo_tb;
     $dumpfile("asynchronous_fifo.vcd");   // name of waveform file
     $dumpvars(0, asynchronous_fifo_tb);   // dump all signals in testbench hierarchy
     reset = 0;
-    write_en = 0;
-    read_en = 0;
-    write_data = 8'h00;
     #2 reset = 1;
     #10 reset = 0;
     repeat (30) @(posedge rd_clk);
@@ -38,19 +39,28 @@ module asynchronous_fifo_tb;
   end
   
   always @(posedge wr_clk) begin
-    if (!reset && !full) begin //after reset deasserted and if FIFO not full
+    if (reset) begin
+      burst_counter <= 0;
+      write_data <= 0;
+      write_en <= 0;
+    end else if (!full && burst_counter < BURST_LENGTH) begin
       write_en <= 1;
       write_data <= write_data + 1;
+      burst_counter <= burst_counter + 1;
+    end else if (burst_counter == BURST_LENGTH) begin
+      write_en <= 0;
+      write_data <= write_data - 1; // to be able to see continuous values
+      burst_counter <= 0;
     end
   end
   
   always @(posedge rd_clk) begin
-    if (reset)
+    if (reset) begin
       read_en <= 0;
-    else begin
+    end else begin
       read_en <= !empty;
       if (read_en && !empty)
-        $display("Read- %0h", read_data);
+        $strobe("Read- %0h", read_data);
     end
   end
   
